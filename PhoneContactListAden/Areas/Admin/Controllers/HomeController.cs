54bd0d3006a90a9b895b830a3b33d2be2420fa0c
jeffjack38 <jeaden38@gmail.com>
2023-09-27 20:06:47 -0500

    Admin update

---
+++ PhoneContactListAden/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneContactListAden.Models;
+
+namespace PhoneContactListAden.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ContactContext context { get; set; }
+
+        public HomeController(ContactContext ctx) =>
+            context = ctx;
+        public IActionResult Index()
+        {
+            var contacts = context.Contacts.OrderBy(
+                c => c.Name).ToList();
+            return View(contacts);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ PhoneContactListAden/Areas/Admin/Models/ContactContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PhoneContactListAden.Models
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().HasData(
+                new Contact
+                {
+                    ContactId = 1,
+                    Name = "Frank",
+                    PhoneNumber = "123-456-7890",
+                    Address = "123 Mulberry Street",
+                    Note = "Frankenstein"
+                },
+
+                new Contact
+                {
+                    ContactId = 2, 
+                    Name = "Billy",
+                    PhoneNumber = "123-456-7891",
+                    Address = "234 Mulberry Street",
+                    Note = "Neighbor"
+                }
+                );
+        }
+    }
+}
